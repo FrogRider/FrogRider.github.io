66ba041a51debea73bfae248cb182842562783e6
FrogRider <jhonny.lbk@gmail.com>
2018-10-23 03:55:16 +0300

    galery

--- hide.js
+++
@@ -1,8 +0,0 @@
-function hide (){
-		document.getElementById('inviz').style.visibility="hidden";
-		document.getElementById('viz').style.visibility="visible";
-		setTimeout(show, 240000);
-	}
-	function show (){
-		document.getElementById('inviz').style.visibility="visible";
-	}
