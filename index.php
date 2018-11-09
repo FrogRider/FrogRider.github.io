801f43edd492472520f3a93fa2fffc553409ce35
FrogRider <jhonny.lbk@gmail.com>
2018-11-09 19:32:38 +0200

    viano firststeps

--- index.php
+++ index.php
@@ -1,51 +1,72 @@
-<!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<!doctype html>
+<html lang="en">
+	<head>
 
-<head>
-	<meta charset="utf-8" />
-	<title>Master groomer</title>
-	<meta content="" name="description" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-	<link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css" />
-	<link rel="stylesheet" href="libs/animate/animate.css" />
-	<?php include ('font.php') ?>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" href="css/gallery.css" />
 	<link rel="stylesheet" href="css/main.css" />
-</head>
 
-<body>
-	<div class="container">
-	<header>
-			<div class="row header">
-					<div class="col-xs-12 col-md-4 logo">
-						<h1> Master groomer</h1>
-					</div>
-					<div class="col-xs-12 col-md-2 about">
-					</div>
-					<div class="col-xs-12 col-md-2 about">
-						<a class="text here" href="index.php">Техника</a>
-					</div>
-					<div class="col-xs-12 col-md-2 about">
-						<a class="text" href="tools.php">Инструменты</a>
-					</div>
-					<div class="col-xs-12 col-md-2 about">
-						<a class="text" href="accessories-categories.php">Аксесуары</a>
-					</div>
-
+	<title>Viano</title>
+	</head>
+	<body>
+		<div class="container-fluid">
+			<header>
+				<div class="row header">
+						<div class="col-xs-12 col-md-4 logo">
+							<h1> Viano Group</h1>
+						</div>
+						<div class="col-xs-12 col-md-2 about">
+						</div>
+						<div class="col-xs-12 col-md-2 about">
+							<a class="text here" href="#">О нас</a>
+						</div>
+						<div class="col-xs-12 col-md-2 about">
+							<a class="text" href="#">Галерея</a>
+						</div>
+						<div class="col-xs-12 col-md-2 about">
+							<a class="text" href="#">Цены</a>
+						</div>
+				</div>
+				<div class="row mobile-header">
+						<div class="topnav">
+						  <a href="#" class="active">Viano Group</a>
+						  <div id="myLinks">
+						    <a href="#">О нас</a>
+						    <a href="#">Цены</a>
+						    <a href="#">Галерея</a>
+						  </div>
+						  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+						    <h6>Menu</h6>
+						  </a>
+						</div>
+<script src = "js/dropdown-menu.js"></script>
+				</div>
+			</header>
+			<div class="gallery">
+				<div class="text-mobile"><hr><h5>REFETTORIO FELIX</h5></div>
+				<div><a href=""><img src="img/viano.png" alt="" style = "max-height: 500px"></a></div>
+				<div class="text-mobile"><hr><h5>REFETTORIO FELIX</h5></div>
+				<div><a href=""><img src="img/viano.png" alt="" style = "max-height: 500px"></a></div>
+				<div class="text-mobile"><hr><h5>REFETTORIO FELIX</h5></div>
+				<div><a href=""><img src="img/viano.png" alt="" style = "max-height: 500px"></a></div>
+				<div class="text-mobile"><hr><h5>REFETTORIO FELIX</h5></div>
+				<div><a href=""><img src="img/viano.png" alt="" style = "max-height: 500px"></a></div>
+				<div class="text-mobile"><hr><h5>REFETTORIO FELIX</h5></div>
+				<div><a href=""><img src="img/viano.png" alt="" style = "max-height: 500px"></a></div>
+				<div class="text-mobile"><hr><h5>REFETTORIO FELIX</h5></div>
+				<div><a href=""><img src="img/viano.png" alt="" style = "max-height: 500px"></a></div>
+				<div class="text-mobile"><hr><h5>REFETTORIO FELIX</h5></div>
+				<div><a href=""><img src="img/viano.png" alt="" style = "max-height: 500px"></a></div>
+				<div class="text-mobile"><hr><h5>REFETTORIO FELIX</h5></div>
+				<div><a href=""><img src="img/viano.png" alt="" style = "max-height: 500px"></a></div>
 			</div>
-	</header>
-
-		<div class="gallery">
-			<script src = "js/product-grid.json"></script>
-			<script src = "js/product-grid.js"></script>
 		</div>
-
-		<?php include ('footer.php') ?>
-	</div>
-	
-	<!-- <script src="js/common.js"></script> -->
-	
-</body>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	</body>
 </html>
+
