cb6c91789b00f8cb17b9be5934a9f01ea82f4249
FrogRider <jhonny.lbk@gmail.com>
2018-12-01 00:35:02 +0200

    fix

--- js/caruselSettings.js
+++ js/caruselSettings.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 	$("#slider_container").owlCarousel({
-		items: 2,
+		items: 1,
 		autoHeight:true,
 		margin:10,
-		loop:true,
+		loop:false,
 		center: true,
-		autoplay:true,
+		autoplay:false,
 	    autoplayTimeout:5000,
 	    autoplayHoverPause:true	
 
