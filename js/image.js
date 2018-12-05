9f4b2934d3ed4acc85b81d05c494917199650fe3
FrogRider <jhonny.lbk@gmail.com>
2018-12-05 03:35:37 +0200

    fixes

--- js/image.js
+++ js/image.js
@@ -5,7 +5,7 @@ $(document).ready(function() { // Ждём загрузки страниц
 	  	var img = $(this); // Получаем изображение, на которое кликнули
 		var src = img.attr('src'); // Достаем из этого изображения путь до картинки
 		$("body").append("<div class='popup' id='picture'>"+ //Добавляем в тело документа разметку всплывающего окна
-						 "<div class='popup_bg'><div class='next arrow'><a href='#' onclick='next()'><img src='img/next.svg' class='ico'></a></div><div class='prev arrow'><a href='#' onclick='prev()'><img src='img/back.svg' class='ico'></a></div></div>"+ // Блок, который будет служить фоном затемненным
+						 "<div class='popup_bg'><div class='cross'><a href='#' onclick='closeImg()'><img src='img/crossWhite.svg' style='width:15px'></a></div><div class='next'><a href='#' onclick='next()'><img src='img/next.svg' class='ico'></a></div><div class='prev '><a href='#' onclick='prev()'><img src='img/back.svg' class='ico'></a></div></div>"+ // Блок, который будет служить фоном затемненным
 						 "<img src='"+src+"' class='popup_img' />"+ // Само увеличенное фото
 						 "</div>"); 
 		$(".popup").fadeIn(500); // Медленно выводим изображение
@@ -40,14 +40,11 @@ $(".image").click(function(){//по клику на картинку
 }); 
 
 $(".popup_img").click(function(){ //закрытие при клике на саму увеличенную картинку
-			$(".popup").fadeOut(500);	
-			setTimeout(function() {
-			  $(".popup").remove();
-			}, 800);
+			console.log("click");
 		});
 
 $(document).keydown(function(e) { //closing at ESC
-    if( e.keyCode > 0 && e.keyCode != 37 && e.keyCode != 39) {//закрываем увеличенную картинку на все кнопки кроме стелки в лево и право
+    if( e.keyCode != 37 && e.keyCode != 39) {//закрываем увеличенную картинку на все кнопки кроме стелки в лево и право
         $(".popup").fadeOut(500)
 			setTimeout(function() {	
 			  $(".popup").remove();
@@ -55,12 +52,11 @@ $(document).keydown(function(e) { //closing at ESC
     } else {
     	var key = e.keyCode; //смотрим, какая кнопка нажата
     	// console.log(e);
-    	
     	if (e.keyCode == 39){//если стрелка в право- листаем в право (прибавляем к индексу картинки)
     		next();
     	} else if (e.keyCode == 37){ //если влево- от индекса картинки отнимаем
     		prev(); 
-    	}
+    	} 
     }
 });
 
@@ -71,7 +67,7 @@ var next = function(){
 	}
     document.getElementById("picture").remove();
     $("body").append("<div class='popup' id='picture'>"+ //Добавляем в тело документа разметку всплывающего окна
-				 	 "<div class='popup_bg'><div class='next arrow'><a href='#' onclick='next()'><img src='img/next.svg' class='ico'></a></div><div class='prev arrow'><a href='#' onclick='prev()'><img src='img/back.svg' class='ico'></a></div></div>"+  // Блок, который будет служить фоном затемненным
+				 	 "<div class='popup_bg'><div class='cross'><a href='#' onclick='closeImg()'><img src='img/crossWhite.svg' style='width:15px'></a></div><div class='next'><a href='#' onclick='next()'><img src='img/next.svg' class='ico'></a></div><div class='prev '><a href='#' onclick='prev()'><img src='img/back.svg' class='ico'></a></div></div>"+  // Блок, который будет служить фоном затемненным
 				 	 "<img src='"+imagesSrcs[picNumber]+"' class='popup_img' />"+ // Само увеличенное фото
 				 	 "</div>");
 				 	 $(".popup").fadeIn(0); 
@@ -84,9 +80,16 @@ picNumber--;
     }
     document.getElementById("picture").remove();
     $("body").append("<div class='popup' id='picture'>"+ //Добавляем в тело документа разметку всплывающего окна
-					 "<div class='popup_bg'><div class='next arrow'><a href='#' onclick='next()'><img src='img/next.svg' class='ico'></a></div><div class='prev arrow'><a href='#' onclick='prev()'><img src='img/back.svg' class='ico'></a></div></div>"+  // Блок, который будет служить фоном затемненным
+					 "<div class='popup_bg'><div class='cross'><a href='#' onclick='closeImg()'><img src='img/crossWhite.svg' style='width:15px'></a></div><div class='next'><a href='#' onclick='next()'><img src='img/next.svg' class='ico'></a></div><div class='prev '><a href='#' onclick='prev()'><img src='img/back.svg' class='ico'></a></div></div>"+ // Блок, который будет служить фоном затемненным
 					 "<img src='"+imagesSrcs[picNumber]+"' class='popup_img' />"+ // Само увеличенное фото
 					 "</div>");
 					 $(".popup").fadeIn(0);
 }
 
+var closeImg = function(){
+	$(".popup").fadeOut(500);	
+			setTimeout(function() {
+			  $(".popup").remove();
+			}, 800);
+	// console.log('close');
+}
