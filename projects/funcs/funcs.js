0309a06a16ebfa9ae346b7934385f5d19d737099
FrogRider <jhonny.lbk@gmail.com>
2019-03-28 15:02:20 +0200

    new func- month()

--- projects/funcs/funcs.js
+++ projects/funcs/funcs.js
@@ -1,6 +1,6 @@
 //goes thru an object searching for matching piece of text/word
 let objectParser = (obj, stack, searchFor, changeTo) => {
-  for (var property in obj) {
+  for (let property in obj) {
     if (obj.hasOwnProperty(property)) {
       if (typeof obj[property] == "object") {
         objectParser(obj[property], stack + '.' + property, searchFor, changeTo);
@@ -15,9 +15,9 @@ let objectParser = (obj, stack, searchFor, changeTo) => {
   }
 }
 
-//checks fo quantity of numbers in selected num input field
+//checks for quantity of numbers in selected num input field
 let numberValueCheck = (id, amount) => {
-  var text = document.getElementById(id);
+  let text = document.getElementById(id);
   text.onkeyup = function testKey() {
     for (i in text.value) {
       if (i >= amount) {
@@ -29,10 +29,10 @@ let numberValueCheck = (id, amount) => {
 
 //returns current date. day, month and year separated by period by default. can be changed
 let today = (separator = '.') => {
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth() + 1; //january is 0
-  var yyyy = today.getFullYear();
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; //january is 0
+  let yyyy = today.getFullYear();
 
   if (dd < 10) {
     dd = '0' + dd;
@@ -42,15 +42,15 @@ let today = (separator = '.') => {
     mm = '0' + mm;
   }
 
-  today = [mm, dd, yyyy].join(separator);
+  today = [dd, mm, yyyy].join(separator);
   return today;
 }
 
 //returns diference between two dates
 let dateDiff = (date1, date2) => {
-  var d1 = new Date(date1);
-  var d2 = new Date(date2);
-  var daysLag = Math.ceil(Math.abs(d2.getTime() - d1.getTime()) / (1000 * 3600 * 24));
+  let d1 = new Date(date1);
+  let d2 = new Date(date2);
+  let daysLag = Math.ceil(Math.abs(d2.getTime() - d1.getTime()) / (1000 * 3600 * 24));
   if (d1 > d2) {
     daysLag = -daysLag;
   }
@@ -59,13 +59,41 @@ let dateDiff = (date1, date2) => {
 
 //takes an id of input field as parametr. checks if only ukrainian letters inputted
 let ukrCheck = (id) => {
-  var text = document.getElementById(id);
+  let text = document.getElementById(id);
   text.onkeyup = function testKey() {
     for (i in text.value) {
       if (text.value[i].match(/[ыЫъЪэЭa-zA-Z]+/) !== null) {
         text.value = text.value.replace(/[ыЫъЪэЭa-zA-Z]+/, '');
-        document.getElementById(id + '_label').style.display = "block";
-      } else document.getElementById(id + '_label').style.display = "none";
+      } 
     }
   }
+}
+
+//returns entered month as a word in one of the languages
+let month = (month, lang = 'en') => {
+  var arr = {
+    ua: ['Ciчень', 'Лютий', 'Березень', 'Квiтень',
+      'Травень', 'Червень', 'Липень', 'Серпень',
+      'Вересень', 'Жовтень', 'Листопад', 'Грудень'
+    ],
+    ru: ['Январь', 'Февраль', 'Март', 'Апрель',
+      'Май', 'Июнь', 'Июль', 'Август',
+      'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь',
+    ],
+    en: ['January ', 'February', 'March', 'April',
+      'May', 'June', 'July', 'August',
+      'September', 'October', 'November', 'Decemrer',
+    ],
+    de: ['Januar', 'Februar', 'März', 'April',
+      'Mai', 'Juni', 'Juli', 'August',
+      'September', 'Oktober', 'November', 'Dezember',
+    ]
+  };
+  let foo = (obj, num) => {
+    for (var prop in obj) {
+      if (prop == lang)
+        return (obj[prop][num - 1])
+    }
+  }
+  return (foo(arr, month))
 }
