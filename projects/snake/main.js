f5a1fb4b89529f9306303645b66f417aaa433112
FrogRider <jhonny.lbk@gmail.com>
2019-05-06 18:39:49 +0300

    new cav project

--- projects/snake/main.js
+++ projects/snake/main.js
@@ -20,73 +20,90 @@ controls: (function(){
   tail = 5;
 }())
 
-function mainLoop(){
-	playerX+=xVelos;
-  playerY+=yVelos;
-  if(playerX < 0){
-  	playerX = tileCount-1;
+function mainLoop() {
+  playerX += xVelos;
+  playerY += yVelos;
+  if (playerX < 0) {
+    playerX = tileCount - 1;
   }
-  if(playerX > tileCount - 1){
-  	playerX = 0;
+  if (playerX > tileCount - 1) {
+    playerX = 0;
   }
-  if(playerY < 0){
-  	playerY = tileCount-1;
+  if (playerY < 0) {
+    playerY = tileCount - 1;
   }
-  if(playerY > tileCount - 1){
-  	playerY = 0;
+  if (playerY > tileCount - 1) {
+    playerY = 0;
   }
-  ctx.fillStyle='#2A2232FF';//bg
-  ctx.fillRect(0,0, canv.width, canv.height); 
+  ctx.fillStyle = "#2A2232FF"; //bg
+  ctx.fillRect(0, 0, canv.width, canv.height);
   scoreRecorder(score);
-    if(dotX==playerX && dotY==playerY){
-  		score++;
-}
+  if (dotX == playerX && dotY == playerY) {
+    score++;
+  }
 
-  ctx.fillStyle='#00C886FF';//snake
-  for(var i=0; i<tailParts.length; i++){
-    ctx.fillRect(tailParts[i].x*gridSize,tailParts[i].y*gridSize, gridSize-tileGap,gridSize-tileGap);
-    if(tailParts[i].x==playerX && tailParts[i].y==playerY && (xVelos + yVelos) != 0){
-      if(!(keys[0] == "down" && keys[1] == "up") && 
-         !(keys[0] == "up" && keys[1] == "down") &&
-         !(keys[0] == "left" && keys[1] == "right") &&
-         !(keys[0] == "right" && keys[1] == "left")){
-        loose()
+  ctx.fillStyle = "#00C886FF"; //snake
+  for (var i = 0; i < tailParts.length; i++) {
+    ctx.fillRect(
+      tailParts[i].x * gridSize,
+      tailParts[i].y * gridSize,
+      gridSize - tileGap,
+      gridSize - tileGap
+    );
+    if (
+      tailParts[i].x == playerX &&
+      tailParts[i].y == playerY &&
+      xVelos + yVelos != 0
+    ) {
+      if (
+        !(keys[0] == "down" && keys[1] == "up") &&
+        !(keys[0] == "up" && keys[1] == "down") &&
+        !(keys[0] == "left" && keys[1] == "right") &&
+        !(keys[0] == "right" && keys[1] == "left")
+      ) {
+        loose();
       }
     }
   }
-  tailParts.push({x:playerX,y:playerY});
-  while(tailParts.length > tail){
-  	tailParts.shift();
+  tailParts.push({ x: playerX, y: playerY });
+  while (tailParts.length > tail) {
+    tailParts.shift();
   }
-  
-  if(dotX==playerX && dotY==playerY){
-    	tail++;
-      dotX=random();
-      dotY=random();
-      for(var i=0; i<tailParts.length; i++){
-        if(tailParts[i].x==dotX && tailParts[i].y==dotY){
-          tail--;
-          dotX=random();
-          dotY=random();
-        }
+
+  if (dotX == playerX && dotY == playerY) {
+    tail++;
+    dotX = random();
+    dotY = random();
+    for (var i = 0; i < tailParts.length; i++) {
+      if (tailParts[i].x == dotX && tailParts[i].y == dotY) {
+        tail--;
+        dotX = random();
+        dotY = random();
       }
     }
-  
-  ctx.fillStyle='#F90F59FF';//dot
-  ctx.fillRect(dotX*gridSize,dotY*gridSize, gridSize-tileGap,gridSize-tileGap);
+  }
+
+  ctx.fillStyle = "#F90F59FF"; //dot
+  ctx.fillRect(
+    dotX * gridSize,
+    dotY * gridSize,
+    gridSize - tileGap,
+    gridSize - tileGap
+  );
 }
 
 const loose = () => {
-  tail=5;
-      if(sessionStorage['hScore'] < score)
-        sessionStorage['hScore'] = score;
-      score=0;
-      if(sessionStorage['hScore'] > 0)
-        document.getElementById('hScore').innerHTML = `Your highest score: ${sessionStorage['hScore']} point`;
-      if(sessionStorage['hScore'] > 1)
-        document.getElementById('hScore').innerHTML += 's';
-      console.log('loose');
-}
+  tail = 5;
+  if (sessionStorage["hScore"] < score) sessionStorage["hScore"] = score;
+  score = 0;
+  if (sessionStorage["hScore"] > 0)
+    document.getElementById("hScore").innerHTML = `Your highest score: ${
+      sessionStorage["hScore"]
+    } point`;
+  if (sessionStorage["hScore"] > 1)
+    document.getElementById("hScore").innerHTML += "s";
+  console.log("loose");
+};
 
 const pause = () => {
   xVelos= yVelos = 0;
