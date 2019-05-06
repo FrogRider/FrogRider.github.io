7cfd728499f7314e5445d659a5b76700290ab446
FrogRider <jhonny.lbk@gmail.com>
2019-05-06 13:31:34 +0300

    Updates

---
+++ precache-manifest.436b2531e5a8c937e917ebd38f79ec7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01973c77668b8e52cfc4d2149eccf1a2",
+    "url": "/FrogRider/FrogRider.github.io/index.html"
+  },
+  {
+    "revision": "7f38a7a0610ac1484ab2",
+    "url": "/FrogRider/FrogRider.github.io/static/css/main.f20a0a69.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/FrogRider/FrogRider.github.io/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "7f38a7a0610ac1484ab2",
+    "url": "/FrogRider/FrogRider.github.io/static/js/main.dd3cdb8a.chunk.js"
+  },
+  {
+    "revision": "dbeadcc667acc2048bc9",
+    "url": "/FrogRider/FrogRider.github.io/static/js/runtime~main.8a6d9153.js"
+  }
+]);
