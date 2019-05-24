54b218bc49c1a472f9f35f0bf7f78cfee1cf257c
Jekos <jhonny.lbk@gmail.com>
2019-05-25 00:16:51 +0300

    strange bug

---
+++ projects/react/weather/precache-manifest.1600953d4786e43f6279a709831b4419 00.16.17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb2065c23ea2855037d79dac5e4e026",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "7f083959fa74727415b3",
+    "url": "/react_app/static/css/main.4c88c3e6.chunk.css"
+  },
+  {
+    "revision": "772e8e6f33ce5be03245",
+    "url": "/react_app/static/js/2.65b9c135.chunk.js"
+  },
+  {
+    "revision": "7f083959fa74727415b3",
+    "url": "/react_app/static/js/main.2cfe83b3.chunk.js"
+  },
+  {
+    "revision": "dd7a70cdeda87e112e79",
+    "url": "/react_app/static/js/runtime~main.cba007e3.js"
+  }
+]);
